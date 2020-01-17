e6d4b274eecc8f78ad84ba779fbace8f45595db2
jpsider <jpsider@gmail.com>
2020-01-17 14:56:27 -0500

    minor change

--- psboot/bin/psboot.ps1
+++ psboot/bin/psboot.ps1
@@ -6,9 +6,9 @@
 # Else Get the SystemName and get the list of Module from the PSBoot endpoint
 # Install the modules
 
-# Start a Job with the PSBoot RestPS endpoint (Copy Routes?)
-## RestPS -> PSBoot Endpoints
-### Status -> Should return 'UP' if the application.ps1 ps process is running
+# Start a Job with the PSBoot RestPS endpoint
+## RestPS -> PSBoot Endpoints (Copy the endpoints first)
+### Status/info/getroutes -> Should return 'UP' if the application.ps1 ps process is running
 
 # Start the application.ps1 file
 ## This file should be pretty straight forward and potentially start a user specified script?
